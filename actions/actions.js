67ca0404c252174c9d29b61e925aadbd290a69a5
hendisantika <hendisantika@yahoo.co.id>
2024-08-08 11:06:04 +0700

    Add contactList in actions.js 🫸🌀✏️📗🐧🐳

--- actions/actions.js
+++ actions/actions.js
@@ -2,3 +2,26 @@
 import MongoConnect from '@/database/mongodb';
 
 MongoConnect();
+
+export async function contactList(params) {
+    try {
+        const limit = 5;
+        const page = params.page || 1;
+        const skip = limit * (page - 1);
+
+        const contacts = await Contact.find()
+            .sort({createdAt: -1})
+            .sort({updatedAt: -1})
+            .limit(limit)
+            .skip(skip);
+        const count = await Contact.find().count();
+        const pages = Math.ceil(count / limit);
+        const newContact = contacts.map((contact) => ({
+            ...contact._doc,
+            _id: contact._doc._id.toString(),
+        }));
+        return {contacts: newContact, pages: pages};
+    } catch (e) {
+        return {error: e.message};
+    }
+}
